91e728be401cb9e78b7be0b0b4077fec64569a64
brownb202 <brownb202@gmail.com>
2020-10-13 21:49:02 -0600

    added routes for API calls

--- server/index.js
+++ server/index.js
@@ -11,7 +11,82 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(`../client/dist`));
 
+//Cart get request
+app.get(`cart/:userSession`, (req, res) => {
+  queries.getCartProducts(function(err, results) {
+    if (err) {
+      console.log('error getting cart items' + err)
+    }
+    res.send(results)
+  }, req.params.userSession)
+})
+
+//get products by id
+app.get(`products/:productId`, (req, res) => {
+  queries.getCartProducts(function(err, results) {
+    if (err) {
+      console.log('error getting product details' + err)
+    }
+    res.send(results)
+  }, req.params.productId)
+})
+
+//get products list
+app.get(`products/list`, (req, res) => {
+  queries.getCartProducts(function(err, results) {
+    if (err) {
+      console.log('error getting product list' + err)
+    }
+    res.send(results)
+  }, req.params.productId)
+})
+
+//get styles by Id
+app.get(`products/productId/styles`, (req, res) => {
+  queries.getCartProducts(function(err, results) {
+    if (err) {
+      console.log('error getting product styles' + err)
+    }
+    res.send(results)
+  }, req.params.productId)
+})
+
+//get ratings for products
+app.get(`products/reviews/productId/meta`, (req, res) => {
+  queries.getCartProducts(function(err, results) {
+    if (err) {
+      console.log('error getting reviews' + err)
+    }
+    res.send(results)
+  }, req.params.productId)
+})
+
+//post items to the cart
+app.post(`cart/:userSession`, (req, res) => {
+  queries.getCartProducts(function(err, results) {
+    if (err) {
+      console.log('error posting to cart' + err)
+    }
+    res.send(results)
+  }, req.params.userSession)
+})
+
+
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Hello, Scrumdog.  Your server is running on PORT: ${PORT}`);
 });
+
+/*
+app.get(`/employeeinfo/:id`, (req, res) => {
+  console.log('the request params are: ', req.params)
+  queries.getEmployeeById(function(err, results) {
+    if (err) {
+      throw err
+    }
+    res.send(results)
+  }, req.params.id)
+})
+
+*/
