1f842e31d695310d9efeb28bc68e1e85c4a65fe6
brownb202 <brownb202@gmail.com>
2020-10-13 21:54:49 -0600

    added queries file in database folder

---
+++ server/database/queries.js
@@ -0,0 +1,31 @@
+//const mongoDB
+
+/*
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'TiltedJointBoolean1564',
+  database: 'employeedatabase'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the server.');
+});
+
+*/
+
+//first query route
+
+//second query route
+
+//so on...
+
+
+/*
+module.exports = {
+}
+*/
