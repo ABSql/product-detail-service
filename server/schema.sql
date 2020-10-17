b500d56068b5652f774e900af3f7e69c62492279
brownb202 <brownb202@gmail.com>
2020-10-17 15:07:30 -0600

    added data generation and modified schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,28 +1,30 @@
+DROP DATABASE IF EXISTS productdetailsdb;
+
 CREATE DATABASE productdetailsdb;
 
-USE productdetailsdb;
+\c productdetailsdb;
 /*mater table that contains the ulk of product detail informaiton*/
 CREATE TABLE IF NOT EXISTS productinfo (
-product_id integer AUTO_INCREMENT UNIQUE PRIMARY KEY,
+product_id SERIAL PRIMARY KEY,
 productname VARCHAR(50),
 slogan VARCHAR(50),
-productdescription VARCHAR(50),
+productdescription VARCHAR(100),
 category VARCHAR(50),
 default_price VARCHAR(50),
 );
 
 /*featrures table references the product_id so that features only get assigned to correct product*/
 CREATE TABLE IF NOT EXISTS features (
-featureid integer AUTO_INCREMENT UNIQUE PRIMARY KEY,
-FOREIGN KEY (product_id) REFERENCES productinfo(product_id),
+featureid SERIAL PRIMARY KEY,
+featureproduct integer REFERENCES productinfo(product_id),
 feature VARCHAR(50),
 feature_value VARCHAR(50)
 );
 
 /*styles table references product_id so that certain styles are only assiciated with certian products*/
 CREATE TABLE IF NOT EXISTS styles (
-style_id integer AUTO_INCREMENT UNIQUE PRIMARY KEY
-FOREIGN KEY (product_id) REFERENCES productinfo(product_id),
+style_id SERIAL PRIMARY KEY
+productstyle integer REFERENCES productinfo(product_id),
 style_name VARCHAR(50),
 original_price VARCHAR(50),
 default BOOLEAN
@@ -32,14 +34,16 @@ default BOOLEAN
 
 /*photos and skus reference the styles table */
 CREATE TABLE IF NOT EXISTS photos (
-photoid integer AUTO_INCREMENT UNIQUE PRIMARY KEY
-FOREIGN KEY (styleid) REFERENCES styles(style_id),
+photoid SERIAL PRIMARY KEY
+phto_style integer REFERENCES styles(style_id),
+photo_url VARCHAR(100),
+thumbnail_url VARCHAR(100)
 );
 
 /*photos and skus reference the styles table */
 CREATE TABLE IF NOT EXISTS skus (
-sku_id integer AUTO_INCREMENT UNIQUE PRIMARY KEY
-FOREIGN KEY (styleid) REFERENCES styles(style_id),
+sku_id integer SERIAL PRIMARY KEY
+sku_style integer REFERENCES styles(style_id),
 XS integer,
 S integer,
 M integer,
@@ -62,17 +66,20 @@ XXL integer,
 
 /*cart has a unique id which should represent the user sessions, should only need to reference the product_ids that the user adds to cart*/
 CREATE TABLE IF NOT EXISTS cart (
-sessionid integer AUTO_INCREMENT UNIQUE PRIMARY KEY
-FOREIGN KEY (product_id) REFERENCES productinfo(product_id),
+sessionid SERIAL PRIMARY KEY
+prod_id integer REFERENCES productinfo(product_id),
 );
 
 /*ratings should reference the product_id that people are leaving reviews for*/
 CREATE TABLE IF NOT EXISTS ratings (
-ratingid integer AUTO_INCREMENT UNIQUE PRIMARY KEY
-FOREIGN KEY (product_id) REFERENCES productinfo(product_id),
+ratingid SERIAL PRIMARY KEY
+rating_product integer REFERENCES productinfo(product_id),
 one integer,
 two integer,
 three integer,
 four integer,
 five integer
 );
+
+INSERT INTO productinfo values('green shoes', 'the best shoes', 'made of carbon', 'shoes', '140');
+INSERT INTO features values ('soles', 'tin');
