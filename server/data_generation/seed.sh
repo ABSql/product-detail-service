ae58f62866ba53bb008d1cecb753e61ba293211b
brownb202 <brownb202@gmail.com>
2020-10-19 14:58:59 -0600

    added seed.sh script, and modified authentication in hba.config

--- server/data_generation/seed.sh
+++ server/data_generation/seed.sh
@@ -6,25 +6,28 @@
 
 # Variable Definitions
 # Path to directory bash script is living
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd .. &&  cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Database Variable Definitions
-DATABASE="blog"
-USER="superuser"
+DATABASE="productdetailsdb"
+USER="postgres"
 
 # Output Filename for Faker File
 OUTPUT="posts.csv"
 FILEPATH="$DIR/$OUTPUT"
 # if parameter 1 is not passed as argument default records to be generated to 1000000
-LINES=${1:-1000000}
+LINES=${1:-10000}
 
 ### Import Our Database ###
 # Dont specify a database since CREATE DATABASE is in schema.sql
+#psql -f schema.sql -h 127.0.0.1 -p 5432 -U postgres
 SCHEMA="$DIR/schema.sql"
+#psql -f $SCHEMA -h 127.0.0.1 -p 5432 -U $USER
 psql -U $USER < $SCHEMA
 
 ### Run Our Generator Script ###
 node generator.js --output=$FILEPATH --lines=$LINES
 
 ### Import Our posts.csv file to seed Database ###
-psql -U $USER -d $DATABASE -c "COPY $DATABASE FROM '$FILEPATH' CSV HEADER;
+psql -U $USER -d $DATABASE -c "COPY $DATABASE FROM '$FILEPATH' CSV HEADER";
+echo 'hello world'
